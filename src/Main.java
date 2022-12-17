77ebff6a9fc4bfd3ec5e779bd6d00b24f7cad4f4
Александр <Alexander.popov.usurt@mail.ru>
2022-12-17 20:13:46 +0500

    Homework1 OOP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car lada = new Car(" ", " ", 1.7, " Желтого ", 2015, " ");
+        Car audi = new Car("Audi ", "A8 50 L TDI quattro ", 3.0, " черного ", 0, " Германии " );
+        Car bmw = new Car("BMW ", "Z8 ", 0, " черного ", 2021, " Германии " );
+        Car kia = new Car("Kia ", "Sportage 4-го поколения ", 2.4, " ", 2018, " Южной Корее " );
+        Car hyundai = new Car("Hyundai", "Avante ", 1.6, " ораньжевого ", 2016, " Южной Корее " );
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
+
+
+
     }
 }
